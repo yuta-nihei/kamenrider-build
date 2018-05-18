004ee2d994f128354fe1aebfc15f4768978eb93f
Yuta <>
2018-05-18 19:07:12 +0900

    Update: コメントを追加

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ let num = 0;
 
 $(function() {
     
+    //ラビットボトル
     $(Rabit).change(function(){
         if ($(this).prop('checked')) {
             $('#rabbit').get(0).play();
@@ -22,6 +23,7 @@ $(function() {
         }
     });
 
+    //タンクボトル
     $(Tank).change(function() {
         if ($(this).prop('checked')) {
             $('#tank').get(0).play();
@@ -32,6 +34,7 @@ $(function() {
         }
     });
 
+    //ゴリラボトル
     $(Gorilla).change(function(){
         if ($(this).prop('checked')) {
             $('#gorilla').get(0).play();
@@ -42,6 +45,7 @@ $(function() {
         }
     });
 
+    //ダイアモンドボトル
     $(Diamond).change(function(){
         if ($(this).prop('checked')) {
             $('#diamond').get(0).play();
@@ -52,6 +56,7 @@ $(function() {
         }
     });
 
+    //スパークリングボトル
     $(Sparkling).change(function(){
         if ($(this).prop('checked')) {
             $('#sparkling').get(0).play();
@@ -62,6 +67,7 @@ $(function() {
         }
     });
 
+    //ハザードトリガー
     $(Hazard).change(function(){
         if ($(this).prop('checked')) {
             $('#hazard').get(0).play();
@@ -74,87 +80,97 @@ $(function() {
 
 
 
-
+    //ラビットタンク
     $(document).change(function() {
         setTimeout(function(){
+            //もしラビットタンクにチェックが入ったら
             if ($("input[name*='rabbit']:checked").prop('checked'),'+',$("input[name*='tank']:checked").prop('checked')) {
-                $('#bestmatch').get(0).play() ;
+                $('#bestmatch').get(0).play() ; //ベストマッチ音声
             }
+            //もしハザードトリガー、ラビットタンクにチェックが入ったら
             if ($("input[name*='hazard']:checked").prop('checked') && $("input[name*='rabbit']:checked").prop('checked') && $("input[name*='tank']:checked").prop('checked')) {
                 $('#bestmatch').get(0).pause() ;
             }
         },3000);
     });
 
+    //ゴリラダイアモンド
     $(document).change(function() {
         setTimeout(function(){
+            //もしゴリラダイアモンドにチェックが入ったら
             if ($("input[name*='gorilla']:checked").prop('checked') && $("input[name*='diamond']:checked").prop('checked')) {
                 $('#bestmatch').get(0).play() ;
             }
         },3000);
     });
 
+    //ハザード ラビットタンク
     $(document).change(function() {
         setTimeout(function(){
+            //もしハザードトリガーとラビットタンクにチェックが入ったら
             if ($("input[name*='hazard']:checked").prop('checked') && $("input[name*='rabbit']:checked").prop('checked') && $("input[name*='tank']:checked").prop('checked')) {
                 $('#superbestmatch').get(0).play() ;
             }
         },3000);
     });
 
+    //レバークリック時
     $('.change').click(function() {
         $(this).data("click", ++num);
         var click = $(this).data("click");
+        //ラビットタンクボトルをクリック済みのとき
         if(($("input[name*='rabbit']:checked").prop('checked') && $("input[name*='tank']:checked").prop('checked'))) {
-            $('#rabitan_change').get(0).play();
+            $('#rabitan_change').get(0).play(); //変身音声を鳴らす
             setTimeout(function(){
-                $('.human.--isNomal').show();
+                $('.human.--isNomal').show(); //ラビットタンクフォームへ変更
             },10000);
         }
+        //ハザード,ラビットタンクをクリック済みのとき
         if ($("input[name*='hazard']:checked").prop('checked') && $("input[name*='rabbit']:checked").prop('checked') && $("input[name*='tank']:checked").prop('checked')) {
             $('#rabitan_change').get(0).pause() ;
             setTimeout(function(){
-                $('.human.--isNomal').hide();
+                $('.human.--isNomal').hide(); //ラビットタンクフォームへ変更
             },10000);
         }
         else {
-            $('.human.--isNomal').hide();
+            $('.human.--isNomal').hide(); //隠す
         }
     });
 
+
     $('.change').click(function() {
         if(($("input[name*='gorilla']:checked").prop('checked') && $("input[name*='diamond']:checked").prop('checked'))) {
-                $('#gorillamond_change').get(0).play();
+                $('#gorillamond_change').get(0).play(); //変身音声を鳴らす
                 setTimeout(function(){
-                    $('.human.--isGorimon').show();
+                    $('.human.--isGorimon').show(); //ゴリラダイアモンドフォームへ変更
                 },12000);
         }
         else {
-            $('.human.--isGorimon').hide();
+            $('.human.--isGorimon').hide(); //隠す
         }
     });
 
     $('.change').click(function() {
         if(($("input[name*='sparkling']:checked").prop('checked'))) {
-                $('#sparkling_change').get(0).play();
+                $('#sparkling_change').get(0).play();　//変身音声を鳴らす
                 setTimeout(function(){
-                    $('.human.--isSparkling').show();
+                    $('.human.--isSparkling').show(); //スパークリングフォーム表示
                 },12000);
         }
         else {
-            $('.human.--isSparkling').hide();
+            $('.human.--isSparkling').hide(); //隠す
         }
     });
 
     $('.change').click(function() {
         if($("input[name*='hazard']:checked").prop('checked') && $("input[name*='rabbit']:checked").prop('checked') && $("input[name*='tank']:checked").prop('checked'))  {
-                $('#rabbittank_hazard_change').get(0).play();
+                $('#rabbittank_hazard_change').get(0).play();　//変身音声を鳴らす
                 setTimeout(function(){
-                    $('.human.--isHazard').show();
+                    $('.human.--isHazard').show(); //ハザードフォーム表示
                 },10000);
         }
         else {
-            $('.human.--isHazard').hide();
+            $('.human.--isHazard').hide(); //隠す
         }
     });
 
